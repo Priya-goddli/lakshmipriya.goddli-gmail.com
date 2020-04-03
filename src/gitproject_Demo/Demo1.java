c9bc3a2d6ae4005c4b74de966c87c1467089289d
laksh <lakshmipriya2001@gmail.com>
2020-04-03 11:41:06 +0530

    First Commit

---
+++ src/gitproject_Demo/Demo1.java
@@ -0,0 +1,15 @@
+package gitproject_Demo;
+import java.util.*;
+public class Demo1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(i*i+n);
+		}
+		
+	}
+
+}
